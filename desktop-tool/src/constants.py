fa945f42ba7c629fe260dbebd2edc864480e0bcc
jay-sharma1 <64980964+jay-sharma1@users.noreply.github.com>
2025-11-30 17:02:27 -0500

    Update constants.py

--- desktop-tool/src/constants.py
+++ desktop-tool/src/constants.py
@@ -157,6 +157,17 @@ class TargetSites(Enum):
             Cardstocks.P10: "Plastic (100%)",
         },
     )
+    PrinterStudioCA = TargetSite(
+        base_url="https://www.printerstudio.ca",
+        starting_url_route="personalized/custom-playing-cards-blank-cards.html",
+        supports_foil=False,
+        cardstock_site_name_mapping={
+            Cardstocks.S30: "Standard (smooth)",
+            Cardstocks.S33: "Superior (smooth)",
+            Cardstocks.M31: "Premium (linen)",
+            Cardstocks.P10: "Plastic (100%)",
+        },
+    )
     PrinterStudioUK = TargetSite(
         base_url="https://www.printerstudio.co.uk",
         starting_url_route="personalised/custom-playing-cards-blank-cards.html",
